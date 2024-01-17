64058db9345743b538d61f0d6d3e76738b7894fe
Layt27 <laytonchetty@gmail.com>
2024-01-17 21:57:14 +0400

    update convert_invert function, add print_image_shape function

--- used_functions.py
+++ used_functions.py
@@ -3,12 +3,14 @@ import os
 import cv2
 from datetime import datetime
 
+# -------------------------------------------------------------------------------------------------------------------
+
 # Functions
 
-# Conversion and inversion of images function
-def convert_invert(directory):
+# Invert colors of images function
+def invert_colors(directory):
     start = datetime.now()
-    print("Commencing conversion and inversion of images...")
+    print("Inverting colors of images...")
 
     folders = os.listdir(directory)
 
@@ -23,25 +25,48 @@ def convert_invert(directory):
             for file in files:
                 # Construct the full path to the image
                 image_path = os.path.join(folder_path, file)
-                # print(image_path)
 
                 # Check if the file is an image file
                 if image_path.lower().endswith((".jpg", ".jpeg", ".png")):
                   # Read the image using OpenCV
                   img = cv2.imread(image_path)
 
-                  # Convert the image to grayscale
-                  grayscale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
                   # Invert the colors of the image (reverse intensity values)
-                  inverted_grayscale_img = cv2.bitwise_not(grayscale_img)
+                  inverted_img = cv2.bitwise_not(img)
 
-                  # Overwrite the original image with the converted and inverted image
-                  cv2.imwrite(image_path, inverted_grayscale_img)
+                  # Overwrite the original image with the inverted image
+                  cv2.imwrite(image_path, inverted_img)
 
   
     duration = datetime.now() - start
-    print("Time taken for conversion and inversion of images: ", duration)
+    print("Time taken for inverting the colors of images: ", duration)
+
+
+# Get image shape of images function
+def print_image_shape(directory):
+    folders = os.listdir(directory)
+
+    # Loop through each folder in the directory
+    for folder in folders:
+        folder_path = os.path.join(directory, folder)
+
+        # Check if it's a directory
+        if os.path.isdir(folder_path):
+            files = os.listdir(folder_path)
+            # Loop through each file in the folder
+            for file in files:
+                # Construct the full path to the image
+                image_path = os.path.join(folder_path, file)
+
+                # Check if the file is an image file
+                if image_path.lower().endswith((".jpg", ".jpeg", ".png")):
+                  # Read the image using OpenCV
+                  img = cv2.imread(image_path)
+
+                  # Get image shape with number of channels
+                  image_shape = img.shape       # This will return a tuple (height, width, channels)
+                  print(f"Folder name: {folder}, File name: {file}, Shape: {image_shape}")
+                  return
 
 
 # Main
@@ -50,4 +75,5 @@ def convert_invert(directory):
 directory = "ascii_dataset"
 
 # Function calls
-convert_invert(directory)
+invert_colors(directory)
+print_image_shape(directory)
