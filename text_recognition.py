e3b74b94ce47a7bfb6f0edefa5efa23efc7fc322
Layt27 <laytonchetty@gmail.com>
2024-01-24 18:20:08 +0400

    feat: Add code to detect characters using easyocr library

--- text_recognition.py
+++ text_recognition.py
@@ -3,6 +3,9 @@
 # import numpy as np
 import pickle
 from keras.models import load_model
+import cv2
+import matplotlib.pyplot as plt
+import easyocr
 
 # -------------------------------------------------------------------------------------------------------------
 
@@ -24,6 +27,32 @@ def text_recognition():
     # # Load model 2
     # model2 = tf.saved_model.load("models/efficientnetb0_model")
 
+    # Code for showing bounding boxes on letters
+    
+    # Load image using OpenCV
+    img_path = "test_imgs/test_img_9.jpg"
+    img = cv2.imread(img_path)
+
+    # Initialize the EasyOCR reader
+    reader = easyocr.Reader(['en'])
+
+    # Use EasyOCR to perform OCR and get bounding boxes
+    results = reader.readtext(img)
+    print(results)
+
+    # Draw bounding boxes on the image
+    for (bbox, text, prob) in results:
+        (top_left, top_right, bottom_right, bottom_left) = bbox
+        top_left = tuple(map(int, top_left))
+        bottom_right = tuple(map(int, bottom_right))
+        cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 2)
+
+    # Display the image with bounding boxes
+    plt.imshow(img)
+    plt.title("Detected Characters")
+    plt.show()
+
+# -------------------------------------------------------------------------------------------------------------
 
 # Main
         
